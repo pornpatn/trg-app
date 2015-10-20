0ee58cb6997e5e25a038eb6d3d99536e34d39c1c
pornpatn <pornpatn@civicresource.com>
2015-10-19 20:17:29 -0700

    Update

--- www/index.php
+++ www/index.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?><!DOCTYPE html>
 <!--
     Copyright (c) 2012-2014 Adobe Systems Incorporated. All rights reserved.
 
@@ -616,5 +621,15 @@
         <script type="text/javascript">
             app.initialize();
         </script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-41323135-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
